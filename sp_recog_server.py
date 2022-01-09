ac7fd304d8c8017af064a858c5fbcde9083fda63
DilyDali69 <dilydali69@gmail.com>
2022-01-09 16:16:38 -0600

    AHHHH EVERYTHING WAS BROKEN

--- sp_recog_server.py
+++ sp_recog_server.py
@@ -1,11 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import speech_recognition as sr
+import pyautogui
+from pynput.keyboard import Key, Controller as KeyboardController, KeyCode
+from pynput.mouse import Button, Controller as MouseController
 
 hostName = "localhost"
 serverPort = 8080
 
 def main():
+    keyboard = KeyboardController()
+    mouse = MouseController()
     class MyServer(BaseHTTPRequestHandler):
         def do_GET(self):
             if __name__ == "__main__":        
@@ -22,8 +27,30 @@ def main():
                         # using google speech recognition
                             text = r.recognize_google(audio_text)
                             print("Text: "+ text)
-                            if "but" in text:
-                                print ("haha you said butt")
+                            
+                            #if I say "die", spam run, teabag, and flashlight button#
+                            if "die" in text:
+                                print ("get fuckt")
+                                pyautogui.press('w')
+                                pyautogui.press('c')
+                                pyautogui.press('f')
+                                #keyboard.press('w', 'c', 'f')
+                                # keyboard.keyUp('w')
+                                keyboard.press('c')
+                                # keyboard.keyUp('c')
+                                keyboard.press('c')
+                                # keyboard.keyUp('c')
+                                keyboard.press('f')
+                                # keyboard.keyUp('f')
+                                keyboard.press('f')
+                                # keyboard.keyUp('f')
+                            
+                            # #elif "journal" in text:
+                            #     print ("nurd")
+                            #     keyboard.press('j')
+                                #Event().wait(5)
+                                #time.sleep(5)
+                                #print ("just checkin")
                             main()
                         except:
                             print("Sorry, I did not get that")
